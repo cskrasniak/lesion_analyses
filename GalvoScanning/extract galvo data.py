584ecd2bf9ad86a589e7a8cb80542b2fd7b46888
Christopher Krasniak <christopher.krasniak@internationalbrainlab.org>
2019-11-04 11:12:27 -0500

    cleaned up
    removed old commented out bits

--- GalvoScanning/extract galvo data.py
+++ GalvoScanning/extract galvo data.py
@@ -4,14 +4,11 @@ Created on Sat Aug 10 11:35:12 2019
 
 @author: chris
 """
-
-## Extract Data
-
 import os
 import pandas as pd
 import numpy as np
-from alf.extractors.biased_trials import extract_all
-subject = "CSK-scan-002"
+from ibllib.io.extractors.biased_trials import extract_all
+subject = "CSK-scan-001"
 #def extractGalvoData(subject):
 trainData = pd.DataFrame(columns= ['feedbackType','contrastLeft','contrastRight','probabilityLeft','choice','rewardVolume','feedback_times','stimOn_times','response_times','iti_dur','goCue_times','goCueTrigger_times','laserX','laserY','laserOn'])
 
@@ -26,16 +23,15 @@ for day in days:
     runs = os.listdir(day)
     training = []
     laserData = []
-    
-    
-    for run in runs:         
-        os.chdir(os.path.join(dataPath,subject,day))
+
+    for run in runs:
+        os.chdir(os.path.join(dataPath, subject, day, run))
         try:
             currpath = os.getcwd()
-            training.append(extract_all(run)) #get behavior data
+            training.append(extract_all(currpath)) #get behavior data
         except:
             pass
-        os.chdir(run)
+       # os.chdir(run)
         laserData.append(np.load("laserData")) #get laser data
 
     useMax = []
@@ -43,11 +39,7 @@ for day in days:
         useMax.append(len(el['feedbackType']))
     training = training[useMax.index(max(useMax))] #only take the longest training session 
     laserData = laserData[useMax.index(max(useMax))] #same for laser
-    #        if np.size(laserData,1)>3:
-#            laserData[] = 'visLeft'
-#            laserData[] = 'visRight'
-#            laserData[] = 'offLeft'
-#            laserData[] = 'offleft'
+
     if max(useMax) <= min(useMax)*10 and max(useMax) != min(useMax):
         print('WARNING, there are two files that are close to the same length in folder:')
         print(currpath)
