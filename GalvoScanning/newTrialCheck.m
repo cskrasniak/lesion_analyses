86007b28f7ac8cafad65d37ec7c68caaf3343c30
Christopher Krasniak <christopher.krasniak@internationalbrainlab.org>
2019-12-10 10:54:15 -0500

    Updated version of analog scanning
    this happened a while ago I just never commited it...

--- GalvoScanning/newTrialCheck.m
+++ GalvoScanning/newTrialCheck.m
@@ -1,7 +1,7 @@
 function newTrialCheck(src,event)
 global s2
 
-       if any(event.Data >= 4)
+       if any(event.Data >= 5)
             s2.startForeground
 %             saved = event.Data;
             src.stop()
